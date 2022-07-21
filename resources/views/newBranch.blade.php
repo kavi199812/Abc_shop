8275ca8615d394b6fbfbe2367e0f5dd1f687185f
kavindu <kavitechsh@gmail.com>
2022-07-21 17:30:13 +0530

    New Branch Update

--- resources/views/newBranch.blade.php
+++ resources/views/newBranch.blade.php
@@ -1,1 +1,12 @@
-<h1>new branch</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is new branch page </h1>
+</body>
+</html>
